473775bc8e8051bbec1e6ecdea12636e5749fe2e
wangzhihuang <zhwang.my@gmail.com>
2020-11-08 17:28:34 +0800

    添加了分类数据修改功能

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -31,4 +31,39 @@ $.ajax({
         // 将拼接好的内容放在页面中
         $('#categoryBox').html(html);
     }
-})
+});
+
+// 通过事件委托为编辑按钮添加点击事件
+$('#categoryBox').on('click', '.edit', function () {
+    // 获取要修改分类数据的id
+    var id = $(this).attr('data-id');
+    // 向服务器端发送ajax请求获取要修改的分类数据
+    $.ajax({
+        type: 'get',
+        url: '/categories/' + id,
+        success: function (response) {
+            // 使用模板引擎展示要修改的分类数据
+            var html = template('modifyCategoryTpl', response);
+            $('#formBox').html(html);
+        }
+    })
+});
+
+// 为修改分类数据表单发生提交行为时
+$('#formBox').on('submit', '#modifyCategory', function() {
+    // 获取表单内容
+    var formData = $(this).serialize();
+    // 获取被修改分类数据的id属性
+    var id = $(this).attr('data-id');
+    // 调用修改分类数据接口发送修改请求
+    $.ajax({
+        type: 'put',
+        url: '/categories/' + id,
+        data: formData,
+        success: function (response) {
+            location.reload();
+        }
+    })
+    // 阻止默认提交行为
+    return false;
+});
