2e74ad2d2f964e6243b93ef294f478dafc423c94
wangzhihuang <zhwang.my@gmail.com>
2020-11-08 12:09:48 +0800

    实现了用户（批量）删除功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -87,3 +87,85 @@ $('#modifyBox').on('submit', '#modifyForm', function () {
     // 阻止表单默认提交
     return false;
 })
+
+// 当删除按钮被点击时
+$('#userBox').on('click', '.delete', function () {
+    // 管理员确认要删除用户
+    if(confirm('您真的要删除用户吗？')) {
+        // 获取要删除用户的id
+        var id = $(this).attr('data-id');
+        $.ajax({
+            type: 'delete',
+            url: '/users/' + id,
+            success: function (response) {
+                location.reload();
+            }
+        })
+    }
+});
+
+// 获取全选按钮
+var selectAll = $('#selectAll');
+// 获取批量删除按钮
+var deleteMany = $('#deleteMany');
+
+// 当全选按钮的状态发生改变时
+selectAll.on('change', function() {
+    // 获取全选按钮当前状态
+    var status = $(this).prop('checked');
+
+    if (status) {
+        // 显示批量删除按钮
+        deleteMany.show();
+    } else {
+        // 隐藏批量删除按钮
+        deleteMany.hide();
+    }
+
+    // 获取所有用户并将用户状态和全选按钮保持一致
+    $('#userBox').find('input').prop('checked', status);
+});
+
+// 当用户前面的复选框状态发生改变时
+$('#userBox').on('change', '.userStatus', function() {
+    // 获取到所有用户 在所有用户中过滤出选中的用户
+    // 判断选中用户的数量和所有用户的数量是否一致
+    // 如果一致 就说明所有的用户都是选中的
+    // 否则就有用户没有被选中
+    var inputs = $('#userBox').find('input');
+    if (inputs.length == inputs.filter(':checked').length) {
+        // alert('all selected')
+        selectAll.prop('checked', true);
+    } else {
+        // alert('not all')
+        selectAll.prop('checked', false);
+    }
+
+    // 如果选中的复选框数量大于0，就说明有选中的复选框
+    if (inputs.filter(':checked').length > 0) {
+        deleteMany.show();
+    } else {
+        deleteMany.hide();
+    }
+});
+
+// 为批量删除按钮添加点击事件
+deleteMany.on('click', function () {
+    var ids = [];
+    // 获取选中的用户
+    var checkedUser = $('#userBox').find('input').filter(':checked');
+    // 循环复选框 从复选框元素上获得data-id属性的值
+    checkedUser.each(function (index, element) {
+        ids.push($(element).attr('data-id'));
+    });
+    
+    if(confirm('您确认进行批量删除操作吗？')) {
+        $.ajax({
+            type: 'delete',
+            url: '/users/' + ids.join('-'),
+            success: function () {
+                location.reload();
+            }
+        })
+    }
+});
